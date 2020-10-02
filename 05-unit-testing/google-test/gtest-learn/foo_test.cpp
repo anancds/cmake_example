d54e364364c245642f8b7d4d1f94789a1a44be8d
anancds <anancds@163.com>
2020-10-02 20:12:10 +0800

    updated

--- 05-unit-testing/google-test/gtest-learn/foo_test.cpp
+++ 05-unit-testing/google-test/gtest-learn/foo_test.cpp
@@ -3,25 +3,34 @@
 //
 #include <gtest/gtest.h>
 #include "foo.h"
-
+#include <ctime>
 
 class fooTest : public ::testing::Test {
-    void SetUp() override
-    {
+protected:
+    void SetUp() override {
         std::cout << "Foo FooEnvironment SetUP" << std::endl;
+        start_time_ = time(nullptr);
     }
 
-    void TearDown() override
-    {
+    void TearDown() override {
         std::cout << "Foo FooEnvironment TearDown" << std::endl;
+        const time_t end_time = time(nullptr);
+
+        // Asserts that the test took no more than ~5 seconds.  Did you
+        // know that you can use assertions in SetUp() and TearDown() as
+        // well?
+        EXPECT_TRUE(end_time - start_time_ <= 5) << "The test took too long.";
     }
+
+    time_t start_time_;
 };
 
 TEST_F(fooTest, abc) {
     Foo foo;
 
+    sleep(1);
     std::cout << "test" << std::endl;
-    EXPECT_EQ(1 , foo.foo(1, 2)) << "failed.";
+    EXPECT_EQ(1, foo.foo(1, 2)) << "failed.";
 }
 
 TEST_F(fooTest, throw) {
@@ -38,8 +47,7 @@ TEST_F(fooTest, Predicate_Assertions) {
 //    EXPECT_PRED2(foo.foo(m, n), m, n);
 }
 
-TEST_F(fooTest, Demo)
-{
+TEST_F(fooTest, Demo) {
     FooType<bool> fooType;
 //    fooType.Bar();
 }
