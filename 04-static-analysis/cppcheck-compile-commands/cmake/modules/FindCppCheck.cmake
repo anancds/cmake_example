9579835ffaef5acc4fbcd817566ae54503ce7d66
anancds <anancds@163.com>
2020-10-03 15:12:17 +0800

    updated

--- 04-static-analysis/cppcheck-compile-commands/cmake/modules/FindCppCheck.cmake
+++ 04-static-analysis/cppcheck-compile-commands/cmake/modules/FindCppCheck.cmake
@@ -21,102 +21,101 @@
 # find the cppcheck binary
 
 # if custom path check there first
-if(CPPCHECK_ROOT_DIR)
-    find_program(CPPCHECK_BIN 
-        NAMES
-        cppcheck
-        PATHS
-        "${CPPCHECK_ROOT_DIR}"
-        NO_DEFAULT_PATH)
-endif()
+if (CPPCHECK_ROOT_DIR)
+    find_program(CPPCHECK_BIN
+            NAMES
+            cppcheck
+            PATHS
+            "${CPPCHECK_ROOT_DIR}"
+            NO_DEFAULT_PATH)
+endif ()
 
 find_program(CPPCHECK_BIN NAMES cppcheck)
 
-if(CPPCHECK_BIN)
+if (CPPCHECK_BIN)
     execute_process(COMMAND ${CPPCHECK_BIN} --version
-                  OUTPUT_VARIABLE CPPCHECK_VERSION
-                  ERROR_QUIET
-                  OUTPUT_STRIP_TRAILING_WHITESPACE)
+            OUTPUT_VARIABLE CPPCHECK_VERSION
+            ERROR_QUIET
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
 
     set(CPPCHECK_THREADS_ARG "-j4" CACHE STRING "The number of threads to use")
     set(CPPCHECK_PROJECT_ARG "--project=${PROJECT_BINARY_DIR}/compile_commands.json")
     set(CPPCHECK_BUILD_DIR_ARG "--cppcheck-build-dir=${PROJECT_BINARY_DIR}/analysis/cppcheck" CACHE STRING "The build directory to use")
     # Don't show these errors
-    if(EXISTS "${CMAKE_SOURCE_DIR}/.cppcheck_suppressions")
+    if (EXISTS "${CMAKE_SOURCE_DIR}/.cppcheck_suppressions")
         set(CPPCHECK_SUPPRESSIONS "--suppressions-list=${CMAKE_SOURCE_DIR}/.cppcheck_suppressions" CACHE STRING "The suppressions file to use")
-    else()
+    else ()
         set(CPPCHECK_SUPPRESSIONS "" CACHE STRING "The suppressions file to use")
-    endif()
+    endif ()
 
     # Show these errors but don't fail the build
     # These are mainly going to be from the "warning" category that is enabled by default later
-    if(EXISTS "${CMAKE_SOURCE_DIR}/.cppcheck_exitcode_suppressions")
+    if (EXISTS "${CMAKE_SOURCE_DIR}/.cppcheck_exitcode_suppressions")
         set(CPPCHECK_EXITCODE_SUPPRESSIONS "--exitcode-suppressions=${CMAKE_SOURCE_DIR}/.cppcheck_exitcode_suppressions" CACHE STRING "The exitcode suppressions file to use")
-    else()
+    else ()
         set(CPPCHECK_EXITCODE_SUPPRESSIONS "" CACHE STRING "The exitcode suppressions file to use")
-    endif()
+    endif ()
 
     set(CPPCHECK_ERROR_EXITCODE_ARG "--error-exitcode=1" CACHE STRING "The exitcode to use if an error is found")
     set(CPPCHECK_CHECKS_ARGS "--enable=warning" CACHE STRING "Arguments for the checks to run")
     set(CPPCHECK_OTHER_ARGS "" CACHE STRING "Other arguments")
     set(_CPPCHECK_EXCLUDES)
 
-    foreach(ex ${CPPCHECK_EXCLUDES})
+    foreach (ex ${CPPCHECK_EXCLUDES})
         list(APPEND _CPPCHECK_EXCLUDES "-i${ex}")
-    endforeach(ex)
+    endforeach (ex)
 
-    set(CPPCHECK_ALL_ARGS 
-        ${CPPCHECK_THREADS_ARG} 
-        ${CPPCHECK_PROJECT_ARG} 
-        ${CPPCHECK_BUILD_DIR_ARG} 
-        ${CPPCHECK_ERROR_EXITCODE_ARG} 
-        ${CPPCHECK_SUPPRESSIONS} 
-        ${CPPCHECK_EXITCODE_SUPPRESSIONS}
-        ${CPPCHECK_CHECKS_ARGS} 
-        ${CPPCHECK_OTHER_ARGS}
-        ${_CPPCHECK_EXCLUDES}
-    )
+    set(CPPCHECK_ALL_ARGS
+            ${CPPCHECK_THREADS_ARG}
+            ${CPPCHECK_PROJECT_ARG}
+            ${CPPCHECK_BUILD_DIR_ARG}
+            ${CPPCHECK_ERROR_EXITCODE_ARG}
+            ${CPPCHECK_SUPPRESSIONS}
+            ${CPPCHECK_EXITCODE_SUPPRESSIONS}
+            ${CPPCHECK_CHECKS_ARGS}
+            ${CPPCHECK_OTHER_ARGS}
+            ${_CPPCHECK_EXCLUDES}
+            )
 
-    if(NOT CPPCHECK_XML_OUTPUT)
-        set(CPPCHECK_COMMAND 
-            ${CPPCHECK_BIN}
-            ${CPPCHECK_ALL_ARGS}
-        )
-    else()
+    if (NOT CPPCHECK_XML_OUTPUT)
         set(CPPCHECK_COMMAND
-            ${CPPCHECK_BIN}
-            ${CPPCHECK_ALL_ARGS}
-            --xml 
-            --xml-version=2
-            2> ${CPPCHECK_XML_OUTPUT})
-    endif()
-    
-endif()
+                ${CPPCHECK_BIN}
+                ${CPPCHECK_ALL_ARGS}
+                )
+    else ()
+        set(CPPCHECK_COMMAND
+                ${CPPCHECK_BIN}
+                ${CPPCHECK_ALL_ARGS}
+                --xml
+                --xml-version=2
+                2> ${CPPCHECK_XML_OUTPUT})
+    endif ()
 
+endif ()
 
 
 # handle the QUIETLY and REQUIRED arguments and set YAMLCPP_FOUND to TRUE if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-    CPPCHECK 
-    DEFAULT_MSG 
-    CPPCHECK_BIN)
+        CppCheck
+        DEFAULT_MSG
+        CPPCHECK_BIN)
 
 mark_as_advanced(
-    CPPCHECK_BIN  
-    CPPCHECK_THREADS_ARG
-    CPPCHECK_PROJECT_ARG
-    CPPCHECK_BUILD_DIR_ARG
-    CPPCHECK_ERROR_EXITCODE_ARG
-    CPPCHECK_SUPPRESSIONS
-    CPPCHECK_CHECKS_ARGS
-    CPPCHECK_OTHER_ARGS)
+        CPPCHECK_BIN
+        CPPCHECK_THREADS_ARG
+        CPPCHECK_PROJECT_ARG
+        CPPCHECK_BUILD_DIR_ARG
+        CPPCHECK_ERROR_EXITCODE_ARG
+        CPPCHECK_SUPPRESSIONS
+        CPPCHECK_CHECKS_ARGS
+        CPPCHECK_OTHER_ARGS)
 
-if(CPPCHECK_FOUND)
+if (CPPCHECK_FOUND)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/analysis/cppcheck)
-    add_custom_target(cppcheck-analysis 
-        COMMAND ${CPPCHECK_COMMAND})
+    add_custom_target(cppcheck-analysis
+            COMMAND ${CPPCHECK_COMMAND})
     message("cppcheck found. Use cppccheck-analysis targets to run it")
-else()
+else ()
     message("cppcheck not found. No cppccheck-analysis targets")
-endif()
+endif ()
